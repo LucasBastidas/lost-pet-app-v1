7b41ff4aae8e63442b0056ded768e85b71ff28ed
Lucas Bastidas <lucaasbastidas@hotmail.com>
2022-07-18 12:50:20 -0300

    arreglando modificar posteo

--- client/components/myPetList/index.ts
+++ client/components/myPetList/index.ts
@@ -84,7 +84,7 @@ export function initMyPetList() {
 				const petId = (e as any).detail.petId;
 				const userId = (e as any).detail.userId;
 				state.setPetIdToUpdate(petId, () => {
-					// console.log("Metí el id");
+					console.log("Metí el id");
 					(formCont as any).style.display = "flex";
 				});
 			});
@@ -109,10 +109,10 @@ export function initMyPetList() {
 					newPetDescription,
 					newPetUrlImage,
 					() => {
-						// console.log(state.data.myPetUpdateData);
-						// console.log("voy a modificar");
+						console.log(state.data.myPetUpdateData);
+						console.log("voy a modificar");
 						state.updateDataOfMyReportedPet(() => {
-							// console.log("modifique mi mascota");
+							console.log("modifique mi mascota");
 							state.getMyReportPets(() => {
 								Router.go("/my-reported-pets");
 								(formCont as any).style.display = "none";
