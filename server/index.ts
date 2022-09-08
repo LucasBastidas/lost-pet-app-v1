5b5f34f1bfbd9fd44dae5e30aa4b226ba98800f4
Lucas Bastidas <lucaasbastidas@hotmail.com>
2022-09-07 23:01:54 -0300

    add cors

--- server/index.ts
+++ server/index.ts
@@ -35,7 +35,7 @@ import { json, where } from "sequelize/types";
 	const app = express();
 	app.use(cors());
 	const port = process.env.PORT || 3000;
-	// console.log("hola", process.env); dasass
+	// console.log("hola", process.env); dasasssfgjjdofgjkdfg
 	const SECRET = process.env.SECRET;
 
 	app.use(express.json({ limit: "50mb" }));
