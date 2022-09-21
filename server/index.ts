f77f6c6c2230944df2b6f852e48c342a926cfeb1
Lucas Bastidas <lucaasbastidas@hotmail.com>
2022-09-21 19:50:01 -0300

    cors

--- server/index.ts
+++ server/index.ts
@@ -33,7 +33,7 @@ import { json, where } from "sequelize/types";
 	}
 	const app = express();
 
-	app.use(cors({ origin: "*" }));
+	app.use(cors());
 
 	const port = process.env.PORT || 3000;
 	// console.log("hola", process.env)//
