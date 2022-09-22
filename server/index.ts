4fdb7b4249532a2c7ee93fb12467b96bdbf59778
Lucas Bastidas <lucaasbastidas@hotmail.com>
2022-09-22 01:18:11 -0300

    cors

--- server/index.ts
+++ server/index.ts
@@ -36,7 +36,7 @@ import { json, where } from "sequelize/types";
 	app.use(cors());
 
 	const port = process.env.PORT || 3000;
-	// console.log("hola", process.env)/
+	// console.log("hola", process.env)//
 	const SECRET = process.env.SECRET;
 
 	app.use(express.json({ limit: "75mb" }));
